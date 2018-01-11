29cefd3bf364abb88335a48f8404a65b9dc0aaab
marco <marco@feynman.herts.ac.uk>
2018-01-11 17:33:32 +0000

    docker: start work on user mapping

---
+++ docker/user-mapping.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Losely based on https://gist.github.com/renzok/29c9e5744f1dffa392cf
+#
+# 
+# Ensure you bind mount home to devel:
+#     -v /home/marco:/home/devel
+#
+# Ensure you set the environment:
+#    HOST_USER_ID=$(id -u) HOST_USER_GID=$(id -g)
+
+
+if [ -z "${USER}" ]; then
+    echo "We need USER to be set!"; exit 100
+fi
+
+# if both not set we do not need to do anything
+if [ -z "${HOST_USER_ID}" -a -z "${HOST_USER_GID}" ]; then
+    echo "Nothing to do here." ; exit 0
+fi
+
+groupadd --gid "${USER_GID}" "${USER}" && \
+    useradd \
+        --uid ${USER_ID} \
+        --gid ${USER_GID} \
+        --create-home \
+        --shell /bin/bash \
+        ${USER}
+
+
+# reset user_id to either new id or if empty old (still one of above
+# might not be set)
+USER_ID=${HOST_USER_ID:=$USER_ID}
+USER_GID=${HOST_USER_GID:=$USER_GID}
+
+LINE=$(grep -F "${USER}" /etc/passwd)
+# replace all ':' with a space and create array
+array=( ${LINE//:/ } )
+
+# home is 5th element
+USER_HOME=${array[4]}
+
+sed -i -e "s/^${USER}:\([^:]*\):[0-9]*:[0-9]*/${USER}:\1:${USER_ID}:${USER_GID}/"  /etc/passwd
+sed -i -e "s/^${USER}:\([^:]*\):[0-9]*/${USER}:\1:${USER_GID}/"  /etc/group
+
+chown -R ${USER_ID}:${USER_GID} ${USER_HOME}
+
+exec su - "${USER}"
